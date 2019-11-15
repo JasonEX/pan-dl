3c1421c80ecdd383c420d91eb959dd946ea9a18c
yuqian <yuqian.yang@stu.xjtu.edu.cn>
2019-11-15 10:10:18 +0800

    fix broken configuration setting

--- aux-files/entry.sh
+++ aux-files/entry.sh
@@ -5,8 +5,11 @@ usermod -o -u ${UID} www-data
 chown -R www-data:www-data /conf
 chown -R www-data:www-data /downloads
 
-sed -i 's#^rpc-secret=.*$#rpc-secret='${RPC_SECRET}'#' /conf/aria2.conf
+sed -i '/^rpc-secret=.*$/d' /conf/aria2.conf
+if [ -n "${RPC_SECRET}" ]; then
+    echo 'rpc-secret='${RPC_SECRET}'' >> /conf/aria2.conf
+fi
 
-su -c /usr/bin/aria2c --conf-path="/conf/aria2.conf" www-data
+su -c '/usr/bin/aria2c --conf-path="/conf/aria2.conf"' www-data
 
-"${@-sh}"
+exec "$@"
